05978c0ca7dac760b92d8e04a81b91442bcc2be5
vaishboy45 <rohitvaishya10471@gmail.com>
2025-06-16 18:39:52 +0530

    Add files via upload

---
+++ SymmetricTree.java
@@ -0,0 +1,39 @@
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return isMirror(root.left, root.right);
+    }
+
+    private boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return true;
+        if (t1 == null || t2 == null) return false;
+
+        return (t1.val == t2.val)
+            && isMirror(t1.left, t2.right)
+            && isMirror(t1.right, t2.left);
+    }
+
+    public static void main(String[] args) {
+        SymmetricTree tree = new SymmetricTree();
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+
+        boolean result = tree.isSymmetric(root);
+        System.out.println("Is the tree symmetric? " + result);
+    }
+}
+
